5b89248b80bb89a41641f24d65708ed1c0c4f8f5
Mikko Keinänen <mikko.keinanen@gmail.com>
2024-10-02 23:02:26 +0300

    Pass input to another screen

--- src/screen/home.rs
+++ src/screen/home.rs
@@ -1,11 +1,14 @@
-use iced::widget::{button, column, text};
+use iced::widget::{button, column, text, text_input};
 
-pub struct Home {}
+pub struct Home {
+    name: String,
+}
 
 #[derive(Debug, Clone)]
 pub enum Message {
     AddGame,
     ViewGames,
+    NameChanged(String),
 }
 
 pub enum Action {
@@ -15,7 +18,9 @@ pub enum Action {
 
 impl Home {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            name: "".to_string(),
+        }
     }
 
     pub fn title(&self) -> String {
@@ -26,19 +31,24 @@ impl Home {
         match message {
             Message::AddGame => {
                 print!("Add game");
-                Action::AddGame("Pac-Man".to_string())
+                Action::AddGame(self.name.clone())
                 // Add game
             }
             Message::ViewGames => {
                 // View games
                 Action::None
             }
+            Message::NameChanged(name) => {
+                self.name = name;
+                Action::None
+            }
         }
     }
 
     pub fn view(&self) -> iced::Element<Message> {
+        let name_input_field = text_input("Enter name", &self.name).on_input(Message::NameChanged);
         let add_button = button("Add Game").on_press(Message::AddGame);
         let header = text("Welcome to Iced Game Collection").size(50);
-        column![header, add_button].into()
+        column![header, name_input_field, add_button].into()
     }
 }
